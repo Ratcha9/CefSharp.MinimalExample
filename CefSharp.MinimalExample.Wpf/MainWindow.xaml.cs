644b2351c9b6fec9c6ab131156291d13511dd738
Robert Schiller <robert.schiller@maximago.com>
2018-02-22 17:30:55 +0100

    Remove CefSharp WPF browser. Replace by Winforms browser. Add Button for taking snapshots of winforms browser

--- CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -1,12 +1,42 @@
-﻿using System.Windows;
-
-namespace CefSharp.MinimalExample.Wpf
+﻿namespace CefSharp.MinimalExample.Wpf
 {
+    using System.Windows;
+    using System.Windows.Controls;
+
     public partial class MainWindow : Window
     {
+        #region Constructors
+
         public MainWindow()
         {
             InitializeComponent();
+            Browser.IsBrowserInitializedChanged += Browser_IsBrowserInitializedChanged;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void Browser_IsBrowserInitializedChanged(object sender, IsBrowserInitializedChangedEventArgs e)
+        {
+            Browser.Load("www.google.com");
+        }
+        #endregion
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var bitmap = ControlSnapshot.Snapshot(Browser);
+            var imagesource = ControlSnapshot.CreateImageFromBitmap(bitmap);
+            Window x = new Window()
+            {
+                Content = new Image() { Source = imagesource },
+                Owner = this,
+                Width = 400,
+                Height = 200,
+                Title = "ControlSnapshot"
+            };
+
+            x.Show();
         }
     }
 }
